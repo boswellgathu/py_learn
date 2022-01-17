1555c7f0f37108571b9235683fee901eded48f57
boswelgathu <boswelgathu@microsoft.com>
2022-01-17 22:16:44 +0300

    refactor-2022

---
+++ 5_tuples/count_in_tuple.py
@@ -0,0 +1,5 @@
+# Write a program that takes a tuple of integers and a number to count,
+#  and returns the number of times that number appears in the tuple
+# If the number isn't in the tuple, you should return 0
+# for more info on this quiz, go to this url: http://www.programmr.com/count-tuple
+
